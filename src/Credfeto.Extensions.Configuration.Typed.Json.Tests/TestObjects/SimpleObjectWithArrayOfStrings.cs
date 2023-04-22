ed6d12fffebdd26b6c73dede5a8a3651fc824ce1
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-04-22 16:46:51 +0100

    Fixed code analysis issue

--- src/Credfeto.Extensions.Configuration.Typed.Json.Tests/TestObjects/SimpleObjectWithArrayOfStrings.cs
+++ src/Credfeto.Extensions.Configuration.Typed.Json.Tests/TestObjects/SimpleObjectWithArrayOfStrings.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace Credfeto.Extensions.Configuration.Typed.Json.Tests.TestObjects;
 
 public sealed class SimpleObjectWithArrayOfStrings
 {
+    [SuppressMessage(category: "Meziantou.Analyzers", checkId: "MA0109: Add an overload with Span<T>", Justification = "Would not work here")]
     [JsonConstructor]
     public SimpleObjectWithArrayOfStrings(string[] items)
     {
