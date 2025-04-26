3e1bd007bde717291d516bdf9419935b93d80efc
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 13:26:07 +0000

    Cleanup: SimpleObjectWithOneInt64PropertyValidator.cs

--- src/Credfeto.Extensions.Configuration.Typed.Json.Tests/TestValidators/SimpleObjectWithOneInt64PropertyValidator.cs
+++ src/Credfeto.Extensions.Configuration.Typed.Json.Tests/TestValidators/SimpleObjectWithOneInt64PropertyValidator.cs
@@ -3,5 +3,4 @@ using FluentValidation;
 
 namespace Credfeto.Extensions.Configuration.Typed.Json.Tests.TestValidators;
 
-public sealed class SimpleObjectWithOneInt64PropertyValidator
-    : AbstractValidator<SimpleObjectWithOneInt64Property> { }
+public sealed class SimpleObjectWithOneInt64PropertyValidator : AbstractValidator<SimpleObjectWithOneInt64Property> { }
